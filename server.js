5c2d6bd327ad80e9f403a4faa39c2db7a4a24151
Sei <reneebernardflores@gmail.com>
2026-04-13 23:00:34 +0800

    Modified app.listen

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ mongoose.connect(MONGO, {
   startScheduler({ Borrowed, Student });  
   await verifyMailer();    
 
-  app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+  app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 }).catch(err => {
   console.error("❌ MongoDB connection error:", err.message);
 });
